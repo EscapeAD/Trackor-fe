fcf0b21c21a97a457e246446f090856207b2e79b
Adam Tse <mr.adam.tse@gmail.com>
2016-12-29 16:40:57 -0500

    +router-outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routes.module';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, AppRoutingModule ],
+  declarations: [ AppComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
