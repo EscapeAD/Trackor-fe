e541e8c96acda42a26ad3d5ebfb28486eaaa3396
Adam Tse <mr.adam.tse@gmail.com>
2016-12-30 00:05:58 -0500

    added dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DashBoardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routes.module';
 
 @NgModule({
   imports:      [ BrowserModule, AppRoutingModule ],
-  declarations: [ AppComponent, HomeComponent ],
+  declarations: [ AppComponent, HomeComponent, DashBoardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
