65bb7ddd84e3a6d658f5cddb8ece5a8c8975c1cc
Adam Tse <mr.adam.tse@gmail.com>
2016-12-30 03:00:43 -0500

    +calender part 1 component

---
+++ app/dashboard/calender/calender.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+ moduleId: module.id,
+ selector: 'calender',
+ templateUrl: 'calender.component.html',
+ styleUrls: [],
+ encapsulation: ViewEncapsulation.None
+})
+export class CalenderComponent {}
