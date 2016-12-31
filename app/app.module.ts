d19ee981bde91efb411f9434b95342ffec2bec07
Adam Tse <mr.adam.tse@gmail.com>
2016-12-30 23:12:30 -0500

    +homepage components and footer component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DashBoardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routes.module';
+
 
 @NgModule({
   imports:      [ BrowserModule, AppRoutingModule ],
-  declarations: [ AppComponent, HomeComponent, DashBoardComponent ],
+  declarations: [ AppComponent, HomeComponent, DashBoardComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'page-footer',
+  templateUrl: 'footer.component.html'
+})
+export class FooterComponent {}
